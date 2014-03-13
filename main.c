0cea2f3336c270b904aa3ce184494a967f1b714e
Levente Kurusa <levex@linux.com>
2014-03-13 16:53:27 +0100

    main: read the file in
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- main.c
+++ main.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 void print_help()
 {
 	printf("LevPP - A compiler for the Lev++ programming language\n");
 	printf("Syntax: levpp [--help] file\n");
 	return;
+}
+
+int process_file(char *filename)
+{
+	struct stat st;
+	stat(filename, &st);
+	if (!st.st_size)
+		return 1;
+	
+	uint8_t *file_buf = (uint8_t *)malloc(st.st_size);
+	if (!file_buf)
+		return 1;
+
+	FILE *fp = fopen(filename, "r");
+	if (!fp) {
+		free(file_buf);
+		return 1;
+	}
+
+	size_t len = fread(file_buf, sizeof(char), st.st_size, fp);
+	if (!len) {
+		printf("%s couldn't be read.\n", filename);
+		free(file_buf);
+		return 1;
+	}
+
+	printf("%s", file_buf);
+
 }
 
 int main(int argc, char *argv[])
@@ -20,6 +53,6 @@ int main(int argc, char *argv[])
 		print_help();
 		return 0;
 	}
-
-	return 0;
+	
+	return process_file(argv[1]);
 }
