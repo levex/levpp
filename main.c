857ea045ff2d18e067cbcc8675367f56ed4cd658
Levente Kurusa <levex@linux.com>
2014-03-13 16:40:32 +0100

    main: add help option
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+void print_help()
+{
+	printf("LevPP - A compiler for the Lev++ programming language\n");
+	printf("Syntax: levpp [--help] file\n");
+	return;
+}
+
 int main(int argc, char *argv[])
 {
-	printf("Hello, world!\n");
-	return 0; 
+	if (argc != 2) {
+		print_help();
+		return 0;
+	}
+	
+	if (!strcmp(argv[1], "--help") ||
+		!strcmp(argv[1], "-h") ||
+		!strcmp(argv[1], "-?")) {
+		print_help();
+		return 0;
+	}
+
+	return 0;
 }
