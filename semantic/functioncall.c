3b305e5cb6b0dec691c26745589fa21b9264b2a7
Levente Kurusa <levex@linux.com>
2014-03-15 11:01:19 +0100

    make: add configure script and allow out-of-tree build
    Signed-off-by: Levente Kurusa <levex@linux.com>

---
+++ semantic/functioncall.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <levpp/ast.h>
+#include <levpp/token.h>
+
+/*
+ * syntax of a function call
+ * 
+ * <function>(<arg_list>);
+ */
+int try_parse_call(char *line, ast_t *ast)
+{
+	
+}
